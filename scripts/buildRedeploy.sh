9d401f148a271d3bf46f1bb7174679f7699629d8
Orzech <ozh204@gmail.com>
2017-09-25 00:09:27 +0200

    Przeróbki

--- scripts/buildRedeploy.sh
+++ scripts/buildRedeploy.sh
@@ -3,6 +3,6 @@
 echo "************ UNDEPLOYING *******************"
 ~/glassfish4/glassfish/bin/asadmin undeploy TJE2
 echo "************ BUILDING **********************"
-mvn clean package
+mvn clean package -Dmaven.test.skip=true
 echo "************ DEPLOYING *********************"
 ~/glassfish4/glassfish/bin/asadmin deploy target/TJE2.war
